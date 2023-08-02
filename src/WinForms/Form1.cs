a50d1de1049aada1403bb96e60ed98a513666a6b
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-08-02 16:41:40 +0700

    login gui

---
+++ src/WinForms/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
