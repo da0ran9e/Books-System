b2f3d4ba450c95460eece06d949c121465b25a9e
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-08-17 09:01:06 +0700

    draft

---
+++ src/WinForms/Drawing/SvgRender/SvgElements/SvgCircle.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using WinForms.Drawing.SvgRender;
+
+namespace WinForms.Drawing.SvgRender.SvgElements
+{
+    [SvgElement("circle")]
+    public class SvgCircle : SvgElement
+    {
+        public override string TargetName
+        {
+            get { return "circle"; }
+        }
+
+        public override bool AllowElementDraw
+        {
+            get { return true; }
+        }
+
+        public float CX { get; set; }
+        public float CY { get; set; }
+        public float R { get; set; }
+
+        protected internal override void OnInitAttribute(string strName, string strValue)
+        {
+            switch (strName)
+            {
+                case "cx":
+                    CX = SvgAttributes.GetSize(this, "cx");
+                    break;
+                case "cy":
+                    CY = SvgAttributes.GetSize(this, "cy");
+                    break;
+                case "r":
+                    R = SvgAttributes.GetSize(this, "r");
+                    break;
+            }
+        }
+
+        public override GraphicsPath GetPath()
+        {
+            GraphicsPath gp = new GraphicsPath();
+            RectangleF rectF = new RectangleF(
+                SvgAttributes.GetSize(CurrentParent, "x") + CX - R,
+                SvgAttributes.GetSize(CurrentParent, "y") + CY - R,
+                R * 2,
+                R * 2);
+            gp.AddEllipse(rectF);
+            return gp;
+        }
+
+        protected internal override void Dispose() { }
+    }
+}
---
+++ src/WinForms/Drawing/SvgRender/SvgElements/SvgG.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing.Drawing2D;
+using WinForms.Drawing.SvgRender;
+
+namespace WinForms.Drawing.SvgRender.SvgElements
+{
+    [SvgElement("g")]
+    public class SvgG : SvgElement
+    {
+        public override string TargetName
+        {
+            get { return "g"; }
+        }
+        public override bool AllowElementDraw
+        {
+            get { return true; }
+        }
+        protected internal override void OnInitAttribute(string strName, string strValue) { }
+
+        public override GraphicsPath GetPath()
+        {
+            return null;
+        }
+
+        protected internal override void Dispose() { }
+    }
+}
