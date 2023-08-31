28670c34f616cf9ea3546d226ba2c070501c5571
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-08-31 09:00:59 +0700

    move class outside the layout

---
+++ src/WinForms/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms
+{
+    public class Book
+    {
+        public int index { get; set; }
+        public string isbn { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public int year { get; set; }
+        public string sURL { get; set; }
+        public string mURL { get; set; }
+        public string lURL { get; set; }
+
+        public Book()
+        {
+            this.index = 0;
+            this.isbn = null;
+            this.title = null;
+            this.author = null;
+            this.publisher = null;
+            this.year = 0;
+            this.sURL = null;
+            this.mURL = null;
+            this.lURL = null;
+        }
+
+        public Book(int index, string isbn, string title, string author, string publisher, int year, string sURL, string mURL, string lURL)
+        {
+            this.index = index;
+            this.isbn = isbn;
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.year = year;
+            this.sURL = sURL;
+            this.mURL = mURL;
+            this.lURL = lURL;
+        }
+    }
+}
--- src/WinForms/MainForm.cs
+++ src/WinForms/MainForm.cs
@@ -225,44 +225,44 @@ namespace WinForms
         //
         //Book object
         //
-        public struct Book
-        {
-            public int index { get; set; }
-            public string isbn { get; set; }
-            public string title { get; set; }
-            public string author { get; set; }
-            public string publisher { get; set; }
-            public int year { get; set; }
-            public string sURL { get; set; }
-            public string mURL { get; set; }
-            public string lURL { get; set; }
+        //public struct Book
+        //{
+        //    public int index { get; set; }
+        //    public string isbn { get; set; }
+        //    public string title { get; set; }
+        //    public string author { get; set; }
+        //    public string publisher { get; set; }
+        //    public int year { get; set; }
+        //    public string sURL { get; set; }
+        //    public string mURL { get; set; }
+        //    public string lURL { get; set; }
 
-            public Book()
-            {
-                this.index = 0;
-                this.isbn = null;
-                this.title = null;
-                this.author = null;
-                this.publisher = null;
-                this.year = 0;
-                this.sURL = null;
-                this.mURL = null;
-                this.lURL = null;
-            }
+        //    public Book()
+        //    {
+        //        this.index = 0;
+        //        this.isbn = null;
+        //        this.title = null;
+        //        this.author = null;
+        //        this.publisher = null;
+        //        this.year = 0;
+        //        this.sURL = null;
+        //        this.mURL = null;
+        //        this.lURL = null;
+        //    }
 
-            public Book(int index, string isbn, string title, string author, string publisher, int year, string sURL, string mURL, string lURL)
-            {
-                this.index = index;
-                this.isbn = isbn;
-                this.title = title;
-                this.author = author;
-                this.publisher = publisher;
-                this.year = year;
-                this.sURL = sURL;
-                this.mURL = mURL;
-                this.lURL = lURL;
-            }
-        }
+        //    public Book(int index, string isbn, string title, string author, string publisher, int year, string sURL, string mURL, string lURL)
+        //    {
+        //        this.index = index;
+        //        this.isbn = isbn;
+        //        this.title = title;
+        //        this.author = author;
+        //        this.publisher = publisher;
+        //        this.year = year;
+        //        this.sURL = sURL;
+        //        this.mURL = mURL;
+        //        this.lURL = lURL;
+        //    }
+        //}
 
         //
         //Use Sql query to get book information from Database
