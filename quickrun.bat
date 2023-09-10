8112e8ef2f07992b05830e454175de192fbad54e
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-09-10 13:21:44 +0700

    check index

--- quickrun.bat
+++ quickrun.bat
@@ -2,6 +2,10 @@
 echo Quick run the system.
 echo -----------------------------
 start cmd /c "pip install -r requirements.txt && python backend\drf_book\manage.py migrate && python backend\drf_book\manage.py runserver && pause"
-start cmd /c "src\WinForms\bin\Release\net6.0-windows\Winforms.exe&& pause"
+start cmd /c "cd src\WinForms\bin\Release\net6.0-windows && Winforms.exe && pause"
 echo -----------------------------
+echo System may have optimal recommendations on these user
+echo smarnes71          z5lM&i
+echo hnussen52          d6o0YMqd
+echo tkerwine2          q5O4L.lY0
 pause
